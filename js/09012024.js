5e992a0e3f426a789766a21246d1b0f4520fde3a
ramkicad01 <ramkisridharcad01@gmail.com>
2024-01-10 14:45:58 +0530

    Javascript learning files

---
+++ js/09012024.js
@@ -0,0 +1,51 @@
+/* let mark = prompt("Enter your mark: ");
+function gradecheck(){
+    if (mark!=null){
+        if (mark<35)
+            console.log("FAIL\nBETTER LUCK NEXT TIME")
+        else if (mark>35 && mark<=50)
+            console.log("GRADE C")
+        else if (mark>50 && mark<=75)
+            console.log("GRADE B")
+        else if (mark>75 && mark<=90)
+            console.log("GRADE A")
+        else if (mark>90)
+            console.log("GRADE O\nCONGRATULATIONS!!!")
+    }
+    else 
+        alert("Please enter your mark!")
+}
+gradecheck(mark);  */
+
+
+/* var add1=(a,b)=>{
+    return a+b;
+}
+
+console.log("added sum", add1(parseInt(prompt("Value A")),parseInt(prompt("Value B"))));   */  
+
+/* function switchoperation(){
+    switch(regNo){
+        case ('451'):
+            console.log("Your Hostel is A")
+            break; 
+        
+        case ('751'):
+            console.log("Your Hostel is B")
+            break;
+
+        default:
+            console.log("Enter 451");
+    }
+}
+let regNo = prompt("Enter your Reg. No: ")
+switchoperation(regNo);
+ */
+
+/* let age = prompt("Enter your age: ")
+if(age>24)
+    console.log("You are "+(age-24)+" years older than me")
+else if(age==24)
+    console.log("We both are of same age")
+else 
+    console.log("You are "+(24-age)+" years younger than me") */
---
+++ js/scriptjs.js
@@ -0,0 +1,18 @@
+let a = 30
+let b = 48
+console.log(a+b);
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(a^b)
+
+function evaluation(n){
+    if (n>35)
+        console.log("PASS")
+    else 
+        console.log("FAIL")
+}
+
+evaluation(a);
+evaluation(b);
